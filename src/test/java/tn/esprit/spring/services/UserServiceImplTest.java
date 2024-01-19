d1adefd1e9d894a1b2ac373775d57311146a9b93
azizjaziri48 <74764681+azizjaziri48@users.noreply.github.com>
2024-01-19 12:20:12 +0100

    Update UserServiceImplTest.java

--- src/test/java/tn/esprit/spring/services/UserServiceImplTest.java
+++ src/test/java/tn/esprit/spring/services/UserServiceImplTest.java
@@ -1,33 +1,27 @@
 package tn.esprit.spring.services;
 
-
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Methodorderer.OrderAnnotation; 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
-import org.junit. jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodorder;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import tn.esprit.spring.entities.Role;
 import tn.esprit.spring.entities.User;
-import java.text.ParseException;
 
-import java.text.SimpleDateFormat;
-import Java.util.Date;
 import java.util.List;
 
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
 class UserServiceImplTest {
-  @Autowired 
-  IUserService us ; 
-  @Test 
-  @Order(0) 
-  public void TestRetrieveAllUsers(){
-    List<User> listUsers = us.retrieveAllUsers(); 
-    Assertions.AssertEquals(0, listUsers.size());
-  }
+
+    @Autowired
+    IUserService us;
+
+    @Test
+    @Order(0)
+    public void testRetrieveAllUsers() {
+        List<User> listUsers = us.retrieveAllUsers();
+        Assertions.assertEquals(0, listUsers.size()); // Utilisation correcte de assertEquals
+    }
 }
